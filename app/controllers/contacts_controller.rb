d2476408150ebb6db962bdb96e4df6f8188a3ce7
Jeremy Cleaver <jbwc917@gmail.com>
2017-07-30 01:38:47 +0000

    Contact Mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ def create
      name = params[:contact][:name]
      email = params[:contact][:email]
      body = params[:contact][:comments]
-     ContactMailer.contact_email("name, email, body").deliver
+     ContactMailer.contact_email(name, email, body).deliver
      flash[:success] = "Message sent."
      redirect_to new_contact_path
   else
